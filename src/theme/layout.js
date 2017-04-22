dbfd78e1e774736418ff4716d1a51f5eeefcd202
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-04-22 00:34:38 -0700

    setup: sort css props

--- src/theme/layout.js
+++ src/theme/layout.js
@@ -6,12 +6,12 @@ import { MediaMixins, PropMixins, SizingMixins } from './mixins'
 import { BREAKPOINTS } from './constants'
 
 export const ContainerDiv = styled.div`
-  position: absolute
-  top: 0
-  right: 0
-  left: 0
   bottom: 0
+  left: 0
   padding: 1rem
+  position: absolute
+  right: 0
+  top: 0
 
   ${SizingMixins.maxWidthCentered(BREAKPOINTS.extraLarge)}
 `
